e6459e1d25f8314cc11e6b7ed95fc2a1a0d356ac
Julian651 <46717823+Julian651@users.noreply.github.com>
2019-01-23 20:44:46 -0600

    Update Main.java

--- IOP/Main.java
+++ IOP/Main.java
@@ -1,11 +1,6 @@
-import javax.swing.*;
-public class Main extends JFrame {
+public class Main {
     public static void main(String[] args) {
-        JFrame fr = new JFrame();
-        GUI gui = new GUI();
-        fr.setSize(800,600);
-        fr.setJMenuBar(gui.Menu());
-        fr.add(gui);
-        fr.show();
+        GUI run = new GUI();
+        run.GUI();
     }
 }
